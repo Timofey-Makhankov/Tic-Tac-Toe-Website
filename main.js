372cdaf517f647cfeb62a1de792e4b94cd4a0297
Timofey-Makhankov <timofey.makahnkov@noseryoung.com>
2022-10-17 20:50:52 +0200

    Added a Tic Tac Toe Grid

---
+++ main.js
@@ -0,0 +1,4 @@
+const tiles = Array.from(document.querySelectorAll('.tile'));
+const playerDisplay = document.querySelector('.display-player');
+const resetButton = document.querySelector('#reset');
+const announcer = document.querySelector('.announcer');
