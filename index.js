c2dc824c29935463d7e9c8f52dc2a353b2dc66b9
Timofey-Makhankov <timofey.makahnkov@noseryoung.com>
2022-10-19 19:19:55 +0200

    fixing Dark Mode Color Settings

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 // Dark Mode
 let isDarkMode = localStorage.getItem('darkMode');
 const darkModeToggle = document.getElementById('dark_mode_switcher');
+const buttons = document.getElementsByClassName('button');
 
 const enableDarkMode = () => {
   document.body.classList.add('darkmode');
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].classList.add('darkmode');
+  }
   localStorage.setItem('darkMode', 'enabled');
 };
 
 const disableDarkMode = () => {
   document.body.classList.remove('darkmode');
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove('darkmode');
+  }
   localStorage.setItem('darkMode', null);
 };
 
